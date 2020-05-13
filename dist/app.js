c10be5c6a6b1f679c39db0237cb4e379f0042262
Michel Zarpelon <michelzarpe@gmail.com>
2020-05-13 10:49:03 -0300

    first commit

---
+++ dist/app.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+//criar o server
+const server = restify.createServer({
+    name: 'meat-api', version: '1.0.0'
+});
+//***************************PLUGINS**********************************
+//configuracao de plugin para pegar os parametros de url
+server.use(restify.plugins.queryParser());
+server.get('/hello', [(req, resp, next) => {
+        //passa para proxima callback
+        return next();
+        //return next(false) // termina por aqui mesmo
+    }, (req, resp, next) => {
+        resp.json({ message: 'hello' });
+        return next();
+    }]);
+server.get('/info', (req, resp, next) => {
+    resp.json({
+        message: 'Info Cliente',
+        browser: req.userAgent(),
+        method: req.method,
+        url: req.href(),
+        path: req.path(),
+        query: req.query
+    });
+    return next();
+});
+//ouvir em determinada porta
+server.listen(3000, () => {
+    console.log('api running http://localhost:3000');
+});
