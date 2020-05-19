06277e734cef90d033dc148cc42cff643e9e9f95
Michel Zarpelon <michelzarpe@gmail.com>
2020-05-19 13:55:30 -0300

    version json

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -12,8 +12,18 @@ class UsersRouter extends ModelRouter<User>{
             // delete document.password
         })
     }
+
+    findByEmail = (req,resp,next)=>{
+        if(req.query.email){
+            User.find({email:req.query.email}).then(this.renderAll(resp,next)).catch(next)
+        }else{
+            next();
+        }
+    }
     apllyRoutes(application:restify.Server){
-        application.get('/users',this.findAll);
+        //Accept-version no get
+        application.get({path:'/users',version:'2.0.0'},[this.findByEmail,this.findAll]);
+        application.get({path:'/users',version:'1.0.0'},this.findAll);
         application.get('/users/:id',[this.validateId,this.findById]);
         application.post('/users',this.save);
         //put para alterar o recurso inteiro
